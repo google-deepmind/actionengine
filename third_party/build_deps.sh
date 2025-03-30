4e54cd5f999329db2803ac8b103d4c5fe5af8652
Helena Pankov <helenapankov@pm.me>
2025-03-30 02:49:09 +0100

    Refactor build deps, use pybind11_abseil

--- third_party/build_deps.sh
+++ third_party/build_deps.sh
@@ -4,17 +4,27 @@ third_party_root=$(pwd)
 cc_standard=20
 nproc=8
 
+python_exec_prefix=$(python -c "import sys; print(sys.exec_prefix)")
+
+abseil_install_dir="${third_party_root}/build_deps/abseil-cpp"
+boost_install_dir="${third_party_root}/build_deps/boost"
+cppitertools_install_dir="${third_party_root}/build_deps/cppitertools"
+googletest_install_dir="${third_party_root}/build_deps/googletest"
+poco_install_dir="${third_party_root}/build_deps/poco"
+pybind11_install_dir="${third_party_root}/build_deps/pybind11"
+pybind11_abseil_install_dir="${third_party_root}/build_deps/pybind11_abseil"
+
 mkdir -p build_deps/abseil-cpp
 cd abseil-cpp || exit 1
 mkdir -p build
 cd build || exit 1
 cmake \
-  -DABSL_BUILD_TESTING=ON \
-  -DABSL_USE_GOOGLETEST_HEAD=ON \
+  -DABSL_BUILD_TESTING=OFF \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/abseil-cpp" \
+  -DCMAKE_INSTALL_PREFIX="${abseil_install_dir}" \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${abseil_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
@@ -23,11 +33,12 @@ cd googletest || exit 1
 mkdir -p build
 cd build || exit 1
 cmake \
-  -DCMAKE_PREFIX_PATH="${third_party_root}/build_deps/abseil-cpp" \
+  -DCMAKE_PREFIX_PATH="${abseil_install_dir}" \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/googletest" \
+  -DCMAKE_INSTALL_PREFIX="${googletest_install_dir}" \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${googletest_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
@@ -37,11 +48,12 @@ mkdir -p build
 cd build || exit 1
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/boost" \
+  -DCMAKE_INSTALL_PREFIX="${boost_install_dir}" \
   -DBOOST_INCLUDE_LIBRARIES="fiber" \
   -DBOOST_ENABLE_CMAKE=ON \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${boost_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
@@ -51,9 +63,10 @@ mkdir -p build
 cd build || exit 1
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/poco" \
+  -DCMAKE_INSTALL_PREFIX="${poco_install_dir}" \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${poco_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
@@ -63,10 +76,12 @@ mkdir -p build
 cd build || exit 1
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/pybind11" \
-  -DPython_ROOT_DIR="$(python -c "import sys; print(sys.exec_prefix)")" \
+  -DCMAKE_INSTALL_PREFIX="${pybind11_install_dir}" \
+  -DPython_ROOT_DIR="${python_exec_prefix}" \
+  -DPython3_ROOT_DIR="${python_exec_prefix}" \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${pybind11_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
@@ -76,9 +91,10 @@ mkdir -p __build
 cd __build || exit 1
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/cppitertools" \
+  -DCMAKE_INSTALL_PREFIX="${cppitertools_install_dir}" \
   -G "Ninja" \
   .. || exit 1
+rm -rf "${cppitertools_install_dir}"
 cmake --build . --parallel "${nproc}" --target install || exit 1
 cd "${third_party_root}" || exit 1
 
