fbf8913f12f5685b2dab7a88932518f233f51eee
Helena Pankov <helenapankov@pm.me>
2025-05-05 23:20:06 +0100

    Add build tests action (draft)

--- third_party/build_deps.sh
+++ third_party/build_deps.sh
@@ -4,7 +4,7 @@ mkdir -p build_deps
 
 third_party_root=$(pwd)
 cc_standard=20
-nproc=8
+nproc=6
 
 python_exec_prefix=$(python -c "import sys; print(sys.exec_prefix)")
 
@@ -14,12 +14,13 @@ cppitertools_install_dir="${third_party_root}/build_deps/cppitertools"
 googletest_install_dir="${third_party_root}/build_deps/googletest"
 pybind11_install_dir="${third_party_root}/build_deps/pybind11"
 pybind11_abseil_install_dir="${third_party_root}/build_deps/pybind11_abseil"
+build_folder_name="build_gh_actions"
 
 # Google Test
 mkdir -p build_deps/googletest
 cd googletest
-mkdir -p build
-cd build
+mkdir -p "${build_folder_name}"
+cd "${build_folder_name}"
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${googletest_install_dir}" \
@@ -32,10 +33,11 @@ cd "${third_party_root}"
 # Abseil
 mkdir -p build_deps/abseil-cpp
 cd abseil-cpp
-mkdir -p build
-cd build
+mkdir -p "${build_folder_name}"
+cd "${build_folder_name}"
 cmake \
-  -DABSL_BUILD_TESTING=ON \
+  -DABSL_BUILD_TESTING=OFF \
+  -DABSL_BUILD_TEST_HELPERS=ON \
   -DABSL_RUN_TESTS=OFF \
   -DABSL_LOCAL_GOOGLETEST_DIR="${third_party_root}/googletest" \
   -DABSL_FIND_GOOGLETEST=ON \
@@ -51,8 +53,8 @@ cd "${third_party_root}"
 # Boost
 mkdir -p build_deps/boost
 cd boost
-mkdir -p build
-cd build
+mkdir -p "${build_folder_name}"
+cd "${build_folder_name}"
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${boost_install_dir}" \
@@ -67,13 +69,14 @@ cd "${third_party_root}"
 # Pybind11
 mkdir -p build_deps/pybind11
 cd pybind11
-mkdir -p build
-cd build
+mkdir -p "${build_folder_name}"
+cd "${build_folder_name}"
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${pybind11_install_dir}" \
   -DPython_ROOT_DIR="${python_exec_prefix}" \
   -DPython3_ROOT_DIR="${python_exec_prefix}" \
+  -DPYBIND11_TEST=OFF \
   -G "Ninja" \
   ..
 rm -rf "${pybind11_install_dir}"
@@ -83,8 +86,8 @@ cd "${third_party_root}"
 # cppitertools
 mkdir -p build_deps/cppitertools
 cd cppitertools
-mkdir -p __build
-cd __build
+mkdir -p "__${build_folder_name}"
+cd "__${build_folder_name}"
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${cppitertools_install_dir}" \
@@ -96,7 +99,7 @@ cd "${third_party_root}"
 
 # ZMQ
 mkdir -p build_deps/libzmq && cd libzmq
-mkdir -p build && cd build
+mkdir -p "${build_folder_name}" && cd "${build_folder_name}"
 cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/libzmq" \
@@ -105,14 +108,14 @@ cmake \
 cmake --build . --parallel "${nproc}" --target install
 cd "${third_party_root}"
 
-mkdir -p build_deps/cppzmq && cd cppzmq
-mkdir -p build && cd build
-cmake \
-  -DCMAKE_CXX_STANDARD="${cc_standard}" \
-  -DCMAKE_PREFIX_PATH="${third_party_root}/build_deps/libzmq" \
-  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/cppzmq" \
-  -DCPPZMQ_BUILD_TESTS=OFF \
-  -G "Ninja" \
-  ..
-cmake --build . --parallel "${nproc}" --target install
-cd "${third_party_root}"
+#mkdir -p build_deps/cppzmq && cd cppzmq
+#mkdir -p "${build_folder_name}" && cd "${build_folder_name}"
+#cmake \
+#  -DCMAKE_CXX_STANDARD="${cc_standard}" \
+#  -DCMAKE_PREFIX_PATH="${third_party_root}/build_deps/libzmq" \
+#  -DCMAKE_INSTALL_PREFIX="${third_party_root}/build_deps/cppzmq" \
+#  -DCPPZMQ_BUILD_TESTS=OFF \
+#  -G "Ninja" \
+#  ..
+#cmake --build . --parallel "${nproc}" --target install
+#cd "${third_party_root}"
