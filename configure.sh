bba04bc267e48ad1503b2fea33caf6361b656132
Ilia Udalov <iudalov@google.com>
2025-05-18 21:41:31 +0100

    Minor build fixes for vanilla Mac OS 15

--- configure.sh
+++ configure.sh
@@ -1,28 +1,32 @@
+#!/usr/bin/env bash
+
 set -e
 
 cc_standard=20
 
-repo_root=$(pwd)
+repo_root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+deps_dir="${repo_root}/third_party"
+build_dir="${repo_root}/build"
 
 # skip dependencies if specified
 if [[ "$1" == "--skip-deps" ]]; then
   echo "Skipping third-party dependencies build..."
 else
   echo "Updating submodules..."
-  git submodule update --init --recursive
+  (cd "${repo_root}" && git submodule update --init --recursive)
   echo "Building third-party dependencies..."
-  cd third_party
-  CC=clang CXX=clang++ ./build_deps.sh
-  cd "$repo_root"
+  (cd "${deps_dir}" && CC=clang CXX=clang++ ./build_deps.sh)
 fi
 
-mkdir -p build
-cd build
+
+
 echo "Configuring build..."
-CC=clang CXX=clang++ cmake \
+(mkdir -p "${build_dir}" \
+  && cd "${build_dir}" \
+  && CC=clang CXX=clang++ cmake \
   -DCMAKE_CXX_STANDARD="${cc_standard}" \
   -DCMAKE_INSTALL_PREFIX="${repo_root}/install" \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -GNinja \
-  ..
+  ..)
