d18b585d4fadd9cd7db18dfa8533469278c57974
Helena Pankov <helenapankov@pm.me>
2025-06-29 14:35:21 +0100

    Hotfix broken worker pools

--- src/g3_fiber/g3_fiber/thread_pool.h
+++ src/g3_fiber/g3_fiber/thread_pool.h
@@ -15,8 +15,6 @@
 #ifndef THREAD_FIBER_THREAD_POOL_H_
 #define THREAD_FIBER_THREAD_POOL_H_
 
-#include <absl/container/flat_hash_set.h>
-
 #include <atomic>
 #include <thread>
 
@@ -32,16 +30,13 @@ static void EnsureThreadHasScheduler(Args&&... args) {
     return;
   }
 
-  kThreadHasScheduler = true;
   boost::fibers::use_scheduling_algorithm<Algo>(std::forward<Args>(args)...);
+  kThreadHasScheduler = true;
 }
 
 class WorkerThreadPool {
  public:
-  explicit WorkerThreadPool(bool allow_schedule_through_same_thread = false,
-                            bool schedule_only_through_same_thread = false)
-      : allow_schedule_through_same_thread_(allow_schedule_through_same_thread),
-        schedule_only_through_same_thread_(schedule_only_through_same_thread) {}
+  explicit WorkerThreadPool() = default;
 
   void Start(size_t num_threads = std::thread::hardware_concurrency());
 
@@ -54,14 +49,12 @@ class WorkerThreadPool {
     std::thread thread;
   };
 
-  const bool allow_schedule_through_same_thread_;
-  const bool schedule_only_through_same_thread_;
+  static constexpr bool kScheduleOnSelf = true;
 
   eglt::concurrency::impl::Mutex mu_{};
-  std::atomic<size_t> next_worker_idx_{0};
+  std::atomic<size_t> worker_idx_{0};
   absl::InlinedVector<Worker, 16> workers_{};
   absl::InlinedVector<boost::fibers::scheduler*, 16> schedulers_{};
-  absl::flat_hash_set<boost::fibers::scheduler*> scheduler_set_{};
 };
 
 void EnsureWorkerThreadPool();
