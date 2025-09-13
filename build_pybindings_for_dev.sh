35cb37238d1a830885709e3bbe020fe6d942fa90
Helena Pankov <helenapankov@pm.me>
2025-09-13 16:24:21 -0700

    Add protos to Python

--- build_pybindings_for_dev.sh
+++ build_pybindings_for_dev.sh
@@ -7,12 +7,16 @@ repo_root=$(pwd)
 echo "Building project..."
 cd build
 CC=clang CXX=clang++ cmake --build . --parallel "${nproc}" --target actionengine_pybind11
+CC=clang CXX=clang++ cmake --build . --parallel "${nproc}" --target actProto
 CC=clang CXX=clang++ cmake --build . --parallel "${nproc}" --target pybind11_abseil_status_module
 
 echo "Moving compiled files to Python code directory..."
 cd "$repo_root"
 for f in build/src/actionengine_pybind11*.so; do
     cp -f "$f" "py/actionengine/_C${f##*/actionengine_pybind11}"
+done
+for f in build/src/actionengine/proto/*_pb2.py; do
+    cp -f "$f" "py/actionengine/proto/${f##*/}"
 done
 for f in build/src/pybind11_abseil_status_module/pybind11_abseil_status_module.*.so; do
     cp -f "$f" "py/actionengine/status.${f##*/pybind11_abseil_status_module.}"
---
+++ examples/009-py-debug/action_exceptions.py
@@ -0,0 +1,46 @@
+import asyncio
+
+import actionengine
+
+
+async def run_echo(action: actionengine.Action):
+    try:
+        async for piece in action["text"]:
+            await action["response"].put(piece)
+    finally:
+        await action["response"].finalize()
+    raise ValueError("This is a test exception from run_echo")
+
+
+ECHO_SCHEMA = actionengine.ActionSchema(
+    name="echo",
+    inputs=[("text", "text/plain")],
+    outputs=[("response", "text/plain")],
+)
+
+
+async def main():
+    node_map = actionengine.NodeMap()
+
+    echo = actionengine.Action.from_schema(ECHO_SCHEMA, "test-echo")
+    echo.bind_handler(run_echo)
+    echo.bind_node_map(node_map)
+    echo.run()
+
+    for word in ["Hello", "from", "the", "other", "side"]:
+        await echo["text"].put(word)
+    await echo["text"].finalize()
+
+    try:
+        async for part in echo["response"]:
+            print(f"Response: {part}")
+    except Exception as e:
+        print(f"Caught exception from action: {e}")
+
+
+def sync_main():
+    return asyncio.run(main())
+
+
+if __name__ == "__main__":
+    sync_main()
